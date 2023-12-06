697e2996fb80db486b6be0a9a02eefa2829675e7
crimeraaa <crimeraaa@gmail.com>
2023-12-06 18:30:58 +0800

    Got correct solution for Day 2 Part 1 in Lua!

--- 2023/02-cube-conundrum/lua/part1.lua
+++ 2023/02-cube-conundrum/lua/part1.lua
@@ -1,5 +1,5 @@
 -- What games would be possible if this were the contents of the bag?
-BAG = {RED = 12, GREEN = 13, BLUE = 15}
+BAG = {RED = 12, GREEN = 13, BLUE = 14}
 
 ---@param fmt string
 ---@param ... string|number
@@ -14,42 +14,65 @@ function string.trim(subject)
     return subject:gsub("^%s+", ""):gsub("%s+$", "")
 end
 
+-- Get the game number if it fits the criteria of the bag, else 0.
 ---@param line string
 function tokenize_game(line)
     -- Pass explicit capture group to string.match to extract that
     -- local number = tonumber(line:match("Game ([%d]+):"))
     
     -- 3rd arg onwards are any capture groups specified
-    local start, stop, number = line:find("Game ([%d]+):")
-    number = tonumber(number)
-    printf("Game Number: %i (start: %i, stop: %i)\n", number, start, stop)
+    local start, stop, gamenumber = line:find("Game ([%d]+):")
+    gamenumber = tonumber(gamenumber) -- was returned as a string
 
     -- Remainder of string after game# + remove leading/trailing whitespace
     local sets = line:sub(stop + 1):trim()
+    local game = {}
+    
+    -- Don't capture leading/trailing whitepaces!
+    -- Semicolon separated sets
+    for set in sets:gmatch("%s?([^;]+)%s?") do 
+        local results = {red = 0, green = 0, blue = 0}
+        -- Comma spearated elements
+        for cubes in set:gmatch("%s?([^,]+)%s?") do 
+            -- match a char-only sequence
+            local color = cubes:match("[^%a]-(%a+)") 
+            -- match a digit-only squence
+            local count = tonumber(cubes:match("([%d]+)[^%d]-")) 
+            results[color] = results[color] + count
+        end
+        table.insert(game, results)
+    end
 
-    -- Separate sets
-    for set in sets:gmatch("([^;]+)") do
-        set = set:trim() -- Remove leading whitespace
-        printf("Set:\t\"%s\"\n", set)
-        -- Separate individual elements of a given set
-        for cubes in set:gmatch("[^,]+") do
-            cubes = cubes:trim()
-            printf("\tColor: \"%s\"\n", cubes)
+    for i, set in ipairs(game) do
+        -- printf("Set %i: %i red, %i green, %i blue\n", 
+        --         i, 
+        --         set.red, 
+        --         set.green, 
+        --         set.blue)
+        for color, count in pairs(set) do
+            if (count > BAG[string.upper(color)]) then
+                -- printf("Game %i is not possible!\n", gamenumber)
+                -- printf("Set %i has %i %s cubes.\n", i, count, color)
+                return 0
+            end
         end
     end
+    return gamenumber
 end
 
 ---@param argc integer
 ---@param argv string[]
 function main(argc, argv)
     local file = assert(io.open(argv[1] or "../part1.txt", "r"))
+    local sum = 0
     local number = 1
     for line in file:lines("*l") do
-        printf("Line %i:\n\"%s\"\n", number, line)
-        tokenize_game(line)
-        printf("\n")
+        -- printf("Line %i:\t\"%s\"\n", number, line)
+        sum = sum + tokenize_game(line)
+        -- printf("\n")
         number = number + 1
     end
+    printf("Sum: %i\n", sum)
     file:close()
     return 0
 end
