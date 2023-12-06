10d7993ea99ec7abe4ea61ab61d919eac498fa73
crimeraaa <crimeraaa@gmail.com>
2023-12-06 12:36:55 +0800

    Attempt to make substring.c safer, kinda

--- 2023/01-trebuchet/c/substring.c
+++ 2023/01-trebuchet/c/substring.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // @returns Pointer to start of section that contains the substring.
+// @warning Casts away `const` qualifier. Good luck!
 // @note Only works for nul terminated strings. Good luck!
-char *get_substring(const char *str, const char *sub) {
-    while (*str != '\0') {
-        const char *str1 = str; // start w/ current main letter being checked
-        const char *sub1 = sub; // locally scoped, need main `sub` ptr throughout
-
-        // Peek ahead for matching characters in both strings
-        while (*str1 == *sub1 &&  *str1 != '\0' && *sub1 != '\0') {
-            str1++;
-            sub1++;
-        }
-        // Exhausted the substring, probably means we found it!
-        if (*sub1 == '\0') {
-            return str;
-        } 
-        str++; // move on to next letter
+char *get_substring(const char *src, const char *sub) {
+    // Find first occurence of substring's first char in source
+    src = strchr(src, sub[0]);
+    if (src == NULL || src[0] == '\0') {
+        return NULL; // No match of first substring char or got empty string.
     }
-    return NULL;
+
+    const char *copy = src; // Hold copy to return `src` as is
+    char ch_src = *copy; // store in value to lessen ptr dereference
+    char ch_sub = *sub; 
+
+    // Peek ahead for matches
+    while (ch_src != '\0' && ch_sub != '\0' && ch_src == ch_sub) {
+        ch_src = *(copy++); // postfix operand returns original value before 
+        ch_sub = *(sub++); // incrementing the value stared by this variable.
+    }
+
+    // Exhausted substring, assume successfully matched the whole substring.
+    if (ch_sub == '\0') {
+        return (char*)src; // strchr returned ptr to substring start already.
+    }
+    return NULL; // Implied else, too bad!
 }
 
 int main(int argc, char *argv[]) {
@@ -31,9 +38,9 @@ int main(int argc, char *argv[]) {
     const char *source = argv[1];
     const char *substr = argv[2];
     const char *match = get_substring(source, substr);
-    printf("source: \"%s\", substr: \"%s\"\n", source, substr);
+    printf("source: \"%s\"\nsubstring: \"%s\"\n", source, substr);
     if (match == NULL) {
-        printf("No matches for substring \"%s\".\n", substr);
+        printf("No matches of substring found.\n");
     } else {
         printf("get_substring: \"%s\"\n", match);
     }
