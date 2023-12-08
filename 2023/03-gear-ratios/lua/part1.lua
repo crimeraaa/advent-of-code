7e83fadb69d61bc94b0c16414e3fc5f771bf7d57
crimeraaa <crimeraaa@gmail.com>
2023-12-08 21:47:01 +0800

    Doing a lil bit of 2D array math now it seems

--- 2023/03-gear-ratios/lua/part1.lua
+++ 2023/03-gear-ratios/lua/part1.lua
@@ -1,45 +1,5 @@
 require("helpers")
 
-
----@class Partnumber
----@field index integer[] Indexes into the original line of constructed number.
----@field value integer[] Constructed part number itself.
-
-local function dump_numbers(row, line)
-    printf("%-2i : %s : ", row, line)
-    local is_partnumber = false
-    ---@type Partnumber[]
-    local pnums = {}
-
-    local column = 0 -- Current line column/char being read
-    local pcount = 1 -- Current table in `pnums` being written
-    for char in line:gmatch(".") do
-        column = column + 1 -- sort of a hack but prolly works
-        if (tonumber(char)) then
-            if not is_partnumber then
-                is_partnumber = true
-            end
-            if not pnums[pcount] then
-                pnums[pcount] = {index = {}, value = {}}
-            end
-            -- Construct our partnumber from left to right
-            table.insert(pnums[pcount].index, column)
-            table.insert(pnums[pcount].value, tonumber(char))
-        else
-            if is_partnumber then
-                pcount = pcount + 1
-                is_partnumber = false
-            end
-        end
-    end
-    for _, partnumber in ipairs(pnums) do
-        local value = tonumber(table.concat(partnumber.value, ""))
-        local index = tonumber(table.concat(partnumber.index, ""))
-        printf("%i (@ %i), ", value, index)
-    end
-    printf("\n")
-end
-
 -- TODO: Get part numbers directly adjacent/diagonal to non '.' symbols
 ---@param argc integer
 ---@param argv string[]
@@ -51,8 +11,34 @@ local function main(argc, argv)
         return 1
     end
     local schematic = readfile((argc == 1 and argv[1]) or "../part1.txt")
+    local matrix = {} ---@type string[][]
+    -- Populate matrix as if it were a 2D char array
     for row, line in ipairs(schematic) do
-        dump_numbers(row, line)
+        matrix[row] = {} -- new line
+        for char in line:gmatch(".") do
+            table.insert(matrix[row], char)
+        end
+    end
+
+    printf("\nPrinting out <matrix>:\n")
+    for row = 1, #matrix, 1 do
+        local line = matrix[row]
+        local has_symbol = false
+        local x, y = 0, 0
+        for col = 1, #line, 1 do
+            local char = line[col]
+            printf("%s ", char)
+            -- Any non-dot and non-digit is considered a symbol in the problem
+            if char:find("[^.%d]") then
+                has_symbol = true
+                x, y = col, row -- columns = horizontal, rows = vertical
+            end
+        end
+        if has_symbol then
+            printf(" <--LINE WITH SYMBOL {%i, %i}", x, y)
+        end
+        -- TODO: Walk back to all 8 adjacent cells
+        printf("\n")
     end
     return 0
 end
