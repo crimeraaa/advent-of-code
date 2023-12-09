1b33492ec9fffb063643341237ec77ee0d22b520
crimeraaa <crimeraaa@gmail.com>
2023-12-09 14:15:20 +0800

    Bit of cleaning and solved Day 3 Part 2 yippee

--- 2023/03-gear-ratios/lua/helpers.lua
+++ 2023/03-gear-ratios/lua/helpers.lua
@@ -1,6 +1,19 @@
+---@alias int integer
+---@alias str string
+---@alias bool boolean
+
+---@class Symbol
+---@field lineno int 
+---@field column int Index into the line.
+---@field value str|int Symbol character or constructed part number.
+
+
+PEEK_LEFT = -1
+PEEK_RIGHT = 1
+
 -- C-style printing of formatted strings.
----@param fmt string String literal or C-style format string.
----@param ... string|integer Arguments to C-style format string.
+---@param fmt str String literal or C-style format string.
+---@param ... str|int Arguments to C-style format string.
 function printf(fmt, ...)
     io.stdout:write(fmt:format(...))
 end
@@ -9,7 +22,7 @@ end
 local fallback = "C:/Users/crimeraaa/repos/advent-of-code/2023/03-gear-ratios/"
 
 -- Opens a file with `filename` for reading and dumps each line, char by char.
----@param filename string 
+---@param filename str 
 function readfile(filename)
     local file, errmsg = io.open(filename, "r")
     if not file then
--- 2023/03-gear-ratios/lua/part2.lua
+++ 2023/03-gear-ratios/lua/part2.lua
@@ -1,10 +1,10 @@
 require("helpers")
 
 -- Think in terms of an array of horizontal lines.
----@param width integer
----@param height integer
+---@param width int
+---@param height int
 local function new_2Dboolarray(width, height)
-    ---@type boolean[][]
+    ---@type bool[][]
     local bool_array = {}
     for y = 1, height do -- vertical indexes
         bool_array[y] = {}
@@ -15,51 +15,71 @@ local function new_2Dboolarray(width, height)
     return bool_array
 end
 
----@param matrix string[][]
----@param closure fun(row: integer, col: integer, char: string) Update external values.
-local function loop_matrix(matrix, closure)
+---@param matrix str[][]
+---@param update_gears fun(row: int, col: int, char: str) Update external values.
+local function loop_matrix(matrix, update_gears)
     for row, line in ipairs(matrix) do -- vertical indexes
         for col, char in ipairs(line) do -- horizontal indexes
-            closure(row, col, char)
+            update_gears(row, col, char)
         end
         printf("\n")
     end
 end
 
 ---@param gears Symbol[]
----@param closure fun(lineno: integer, column: integer) Update external values.
-local function loop_gearsymbols(gears, closure)
+---@param create_partnumber fun(lineno: int, column: int):int Update external values.
+local function loop_gearsymbols(gears, create_partnumber)
+    local sum = 0
     for _, gear in ipairs(gears) do
         printf("line:%i:%i = '%s': ", gear.lineno, gear.column, gear.value)
-        for y = -1, 1 do -- For offset reasons we need to start at -1
+        local partnum_count = 0
+        -- Start w/ 1 so first partnum mul'd returns itself instead of 0
+        local gear_ratio = 1 
+
+        -- For offset reasons we need to start at -1
+        for y = -1, 1 do 
             local lineno = y + gear.lineno -- offset lineno index into matrix
             for x = -1, 1 do 
                 local column = x + gear.column -- offset col index into line
-                closure(lineno, column)
+                local partnum_value = create_partnumber(lineno, column)
+                if partnum_value > 0 then
+                    partnum_count = partnum_count + 1
+                    -- sum = sum + partnum_value
+                    gear_ratio = gear_ratio * partnum_value
+                end
             end
         end
-        printf("\n")
+
+        printf(": %i adjacent part numbers.\n", partnum_count)
+        if (partnum_count == 2) then
+            sum = sum + gear_ratio
+        end
     end
+    return sum
 end
 
 -- Look at cells to the left and right of our part number.
----@param line string[] An element indexed from `matrix` (`string[][]`)
----@param column integer Index into `line` as if that were a char array.
+-- This is meant to be used inside of the closure of `loop_gearsymbols`.
+---@param matrix str[][]
+---@param lineno int
+---@param column int Index into `line` as if that were a char array.
 ---@param direction -1|1 Pass `PEEK_LEFT` or `PEEK_RIGHT`.
----@param closure fun(digit: integer, offset: integer) Update extarnal values.
-local function loop_peekoffset(line, column, direction, closure)
+---@param peek_adjacent fun(digit: int, offset: int) Create part number based on adjacent cells.
+local function loop_peekoffset(matrix, lineno, column, direction, peek_adjacent)
     local digit = 1
     local offset = direction -- start off -1 or 1 so we offset already
     while (true) do
-        digit = tonumber(line[column + offset])
+        digit = tonumber(matrix[lineno][column + offset])
         if not digit then
             break
         end
-        closure(digit, offset)
+        peek_adjacent(digit, offset)
         offset = offset + direction -- works for both + and -
     end
 end
 
+-- * Part 2: A gear is any '*' character with 2 adjacent part numbers.
+-- * No more, no less!
 ---@param argc integer
 ---@param argv string[]
 local function main(argc, argv)
@@ -78,7 +98,7 @@ local function main(argc, argv)
     loop_matrix(matrix, function(row, col, char) -- Create a closure!
         printf("%s ", char)
         local is_digit = tonumber(char) -- convert so we can do math
-        if (not is_digit) and (char ~= ".") then
+        if (not is_digit) and (char ~= ".") and (char == "*") then
             ---@type Symbol
             local t = {lineno = row, column = col, value = char}
             -- insert in order of [L]eft to [R]ight, going [U]p to [D]own.
@@ -89,33 +109,33 @@ local function main(argc, argv)
     printf("\n")
     
     -- Walk back through cells adjacent to the current cell (matrix)
-    printf("Printing <SYMBOLS> and adjacent <PARTNUMBERS>:\n")
+    printf("Printing <SYMBOLS> and adjacent <PARTNUM>:\n")
 
     -- If we visited a part number already, don't reconstruct it.
     local visited = new_2Dboolarray(#matrix, #matrix[1])
-    local sum = 0
-    loop_gearsymbols(gears, function(lineno, column) -- create a closure!
-        local part_number = tonumber(matrix[lineno][column])
-        -- Current cell (our "center") isn't even a number? Don't bother!
-        if (not part_number) or visited[lineno][column] then
-            return
+    local sum = loop_gearsymbols(gears, function(lineno, column) -- create a closure!
+        local partnum = tonumber(matrix[lineno][column])
+        -- Current cell (our "center") isn't even a number so don't bother
+        if (not partnum) or visited[lineno][column] then
+            return 0
         end
-        local place_value = 10 -- part_number is the 1's place already
 
+        local placeval = 10 -- part_number is the 1's place already
         -- Update part number going left requires correct place value.
-        loop_peekoffset(matrix[lineno], column, PEEK_LEFT, function(digit, offset)
+        loop_peekoffset(matrix, lineno, column, PEEK_LEFT, function(digit, offset)
             visited[lineno][column + offset] = true
-            part_number = part_number + (digit * place_value)
-            place_value = place_value * 10
+            partnum = partnum + (digit * placeval)
+            placeval = placeval * 10
         end)
 
         -- Update part number going right, which is much simpler than left!
-        loop_peekoffset(matrix[lineno], column, PEEK_RIGHT, function(digit, offset)
+        loop_peekoffset(matrix, lineno, column, PEEK_RIGHT, function(digit, offset)
             visited[lineno][column + offset] = true
-            part_number = (part_number * 10) + digit
+            partnum = (partnum * 10) + digit
         end)
-        sum = sum + part_number
-        printf("%i (@%i:%i), ", part_number, lineno, column)
+
+        printf("%i (@%i:%i), ", partnum, lineno, column)
+        return partnum
     end)
     printf("\n<SUM>: %s\n", sum)
     return 0
