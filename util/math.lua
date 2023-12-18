d7e92674d620cc22f5bdc5a3b89d50707ac4e7be
crimeraaa <crimeraaa@gmail.com>
2023-12-18 09:18:11 +0800

    Updated math modules to utilize factories, just for fun

--- util/math.lua
+++ util/math.lua
@@ -39,48 +39,49 @@ function math.multiples(value, start, stop, source)
     return multiples
 end
 
--- Greatest common denominator of a set of values.
----@param x num
----@param y num
----@param ... num
-function math.gcd(x, y, ...)
-    if select("#", x, y, ...) == 2 then
-        local gcd, rem = x, y
-        -- (gcd=12,rem=8)=>(gcd=8,rem=4)=>(gcd=4,rem=0): return gcd
-        while rem ~= 0 do
-            local tmp = gcd
-            gcd = rem
-            rem = tmp % rem
+-- Create a math function that can work with 2 or more Lua `number`s.
+-- It should also be able to iteratively get the result for a list.
+---@param fn fun(x: num, y: num):num
+---@param ini? num Start value of result variable for list algorithm. Default: 1
+function math.fn_factory(fn, ini)
+    -- lists, e.g. using gcd function, are effectively evaluated as:
+    -- 
+    --      fn(16, 12, 8)
+    --      fn(fn(16, 12), 4)
+    --      fn(fn(fn(ini or 1, 16), 12), 4)
+    ---@param x num
+    ---@param y num
+    ---@param ... num
+    return function(x, y, ...)
+        -- Recurse case/base case to avoid creating unnecessary tables
+        if select("#", x, y, ...) == 2 then
+            return fn(x, y)
         end
-        return gcd
+        -- Previous result, ideally get first list value as is via algorithm
+        -- e.g. previous GCD in list be gcd(1, list[1])
+        local result = ini or 1
+        local list = {x, y, ...}
+        for _, value in ipairs(list) do
+            result = fn(result, value)
+        end
+        return result
     end
-    local list = {x, y, ...}
-    local gcd = 0
-    for i, vx, ii, vy in array.two_ipairs(list) do
-        gcd = math.gcd(vx, vy)
-        -- gcd(16,12,8)=>gcd(16,12): 4, resolves to gcd(4,8): 4
-        -- effectively the same as: gcd(gcd(16, 12), 8)
-        list[ii] = gcd
-    end
-    return gcd
 end
 
+-- Greatest common denominator of 2 values/set of values. 
+math.gcd = math.fn_factory(function(x, y) 
+    local gcd, rem = x, y
+    -- (gcd=12,rem=8)=>(gcd=8,rem=4)=>(gcd=4,rem=0): return gcd
+    while rem ~= 0 do
+        local tmp = gcd
+        gcd = rem
+        rem = tmp % rem
+    end
+    return gcd
+end)
+
 -- Least common multiple of a set of values.
----@param x num
----@param y num
----@param ... num
-function math.lcm(x, y, ...)
-    if select("#", x, y, ...) == 2 then
-        -- LCM is just abs(x * y) / gcd(x,y), also works for x=0 or y=0
-        return (x * y) / math.gcd(x, y)
-    end
-    local list = {x, y, ...}
-    local lcm = 0
-    for i, vx, ii, vy in array.two_ipairs(list) do
-        lcm = math.lcm(vx, vy)
-        -- lcm(2,3,5)=>lcm(2,3): 6, resolves to lcm(6,5): 30
-        -- effectively the same as: lcm(lcm(2, 3), 5)
-        list[ii] = lcm
-    end
-    return lcm
-end
+math.lcm = math.fn_factory(function(x, y)
+    -- LCM is just abs(x*y)/gcd(x,y), also works for x=0 and/or y=0
+    return (x * y) / math.gcd(x, y)
+end)
